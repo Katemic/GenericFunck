0b560765083a0043e77140268a130a780f218f35
Katrine <kah003@edu.zealand.dk>
2024-02-07 10:21:12 +0100

    string length

--- GenericFunck/Program.cs
+++ GenericFunck/Program.cs
@@ -12,9 +12,15 @@ List<double> doubleList = new List<double>() { 3.14, 2.718, 12.4564, 2.0 };
 Console.WriteLine("Largest element in doubleList: " + Functions.FindLargest(doubleList)); // Output: 3.14
 Console.WriteLine("Largest element in doubleList: " + Functions.FindLargestCompare(doubleList)); // Output: 3.14
 
-List<string> stringList = new List<string>() { "apple", "banana", "orange", "kiwi" };
+StringLenght stringComparer = new StringLenght();
+List<string> stringList = new List<string>() { "apple", "banana", "orange", "kiwiiiiiiiiiiiiiii" };
 Console.WriteLine("Largest element in stringList: " + Functions.FindLargest(stringList)); // Output: orange
 Console.WriteLine("Largest element in stringList: " + Functions.FindLargestCompare(stringList)); // Output: orange
+stringList.Sort(stringComparer);
+foreach (string str in stringList)
+{
+    Console.WriteLine(str);
+}
 
 // Opgave 3
 List<Car> carList = new List<Car>() { new Car("AB43412", "Toyota", 160), new Car("CC6562", "Volvo", 200) };
---
+++ GenericFunck/StringLenght.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFunck
+{
+    public class StringLenght : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            if (x.Length < y.Length)
+            { return 1; }
+            if (x.Length > y.Length) 
+            {  return -1; }
+            return 0;
+        }
+    }
+}
