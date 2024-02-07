bc570694cc72f183148ef5fd7280b690d3ad51a4
Katrine <kah003@edu.zealand.dk>
2024-02-07 09:31:07 +0100

    Add project files.

---
+++ GenericFunck/Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFunck
+{
+    public class Functions
+    {
+
+        public static int FindLargest(List<int> list)
+        {
+            int largest = list[0];
+
+            foreach (var item in list )
+            {
+                //if (largest == null) 
+                //{
+                //    largest = item;
+                //}
+                if (largest < item) 
+                {
+                    largest = item;
+                }
+            }
+
+            return largest;
+        }
+    }
+}
